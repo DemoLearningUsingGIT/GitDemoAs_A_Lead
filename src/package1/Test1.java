103dfa3cef2975d98aee4b357e2e2d118a7c743b
sss <him0896singh@gmail.com>
2020-09-16 20:12:24 +0530

    First Commit

---
+++ src/package1/Test1.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if (hungry=="yes") {
+			System.out.println("Eat something------");
+		}
+		else{
+			System.out.println("Do something-----");
+		}
+
+	}
+
+}
