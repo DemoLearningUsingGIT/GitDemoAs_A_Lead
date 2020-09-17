12fcf99d038f152115b559b286d3e5dea67636c7
sss <him0896singh@gmail.com>
2020-09-17 19:10:28 +0530

    Second time commit

---
+++ src/package1/Test2.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry="y";
+		if (hungry=="y") {
+			System.out.println("Eat something test2------");
+		}
+		else{
+			System.out.println("Do something test2-----");
+		}
+
+	}
+
+}
