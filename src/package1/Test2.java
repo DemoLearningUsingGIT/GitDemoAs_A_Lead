5c67615c688b0afb4b578723540036754e84a21b
sss <him0896singh@gmail.com>
2020-09-17 19:28:47 +0530

    Third time commit

--- src/package1/Test2.java
+++ src/package1/Test2.java
@@ -5,10 +5,10 @@ public class Test2 {
 	public static void main(String[] args) {
 		String hungry="y";
 		if (hungry=="y") {
-			System.out.println("Eat something test2------");
+			System.out.println("Third time commit------");
 		}
 		else{
-			System.out.println("Do something test2-----");
+			System.out.println("Third time commit-----");
 		}
 
 	}
